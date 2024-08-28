fc4b9db669a6bba0e07008f9849655c8a07a106c
Oleksiy Bodnia <bodnjaal@gmail.com>
2024-08-28 16:44:41 +0300

    add base project settings, create project, add base styles, add icons, add fonts

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Grid3x3 from './components/icons/Grid3x3.jsx';
+
+function App() {
+  return (
+    <>
+      <p>
+        Hello world <Grid3x3 width={20} height={20} />
+      </p>
+    </>
+  );
+}
+
+export default App;
