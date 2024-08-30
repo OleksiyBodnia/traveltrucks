2d7664f799444d45ec223f9a2c608fa59c511c1f
Oleksiy Bodnia <bodnjaal@gmail.com>
2024-08-30 14:03:08 +0300

    add adaptive for photos

--- src/components/shared/NavigationBar/NavigationBar.jsx
+++ src/components/shared/NavigationBar/NavigationBar.jsx
@@ -1,16 +1,21 @@
 import { NavLink } from 'react-router-dom';
 import styles from './NavigationBar.module.css';
 import photo from '../../../assets/Logo.png';
+import photo2x from '../../../assets/Logo@2x.png';
 
 export default function NavigationBar() {
   return (
     <div className={styles.container}>
-      <img src={photo} alt="logotype" className={styles.img} />
+      <picture>
+        <source srcSet={`${photo2x} 2x`} />
+        <img src={photo} alt="logotype" className={styles.img} />
+      </picture>
       <NavLink
         className={({ isActive }) =>
           isActive ? styles.activeLink : styles.link
         }
         to="/"
+        end
       >
         Home
       </NavLink>
@@ -19,6 +24,7 @@ export default function NavigationBar() {
           isActive ? styles.activeLink : styles.link
         }
         to="/catalog"
+        end
       >
         Catalog
       </NavLink>
